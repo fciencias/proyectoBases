b132d84428c437a80e985d3776945b8ab884099f
Jose Galindo Mtz <jose.galindo@ciencias.unam.mx>
2012-12-06 03:51:39 -0600

    mejor diseno, los botones quedan mejor acomodados y cada pestana tiene sus propios botones

--- Calendarizador/src/calendarizador/GUI.java
+++ Calendarizador/src/calendarizador/GUI.java
@@ -17,9 +17,7 @@ public class GUI {
     
     private JFrame marco;
     private JPanel panel_principal;
-    private JPanel botones;
     private JPanel informacion;
-    private JButton crearUsuarioBoton;
     private JTabbedPane pestanas;
     private JMenuBar menu;
     private static int ALTO = 750;
@@ -34,16 +32,12 @@ public class GUI {
         marco.setPreferredSize(new Dimension(ALTO, LARGO));
         //marco.setJmenuBar(creaBarraMenu());
         panel_principal = new JPanel();
-        panel_principal.setPreferredSize(new Dimension(ALTO-50, LARGO-50));
+        panel_principal.setPreferredSize(new Dimension(ALTO, LARGO));
         panel_principal.setLayout(new BorderLayout());
-        crearUsuarioBoton = new JButton("Crear Usuario");
-        botones = new JPanel();
-        botones.add(crearUsuarioBoton);
         informacion = new JPanel();
         pestanas = creaPestanas();
         informacion.add(pestanas);
-        panel_principal.add(informacion, BorderLayout.WEST);
-        panel_principal.add(botones, BorderLayout.EAST);
+        panel_principal.add(informacion, BorderLayout.CENTER);
         marco.add(panel_principal);
         marco.pack();
     }
@@ -69,16 +63,18 @@ public class GUI {
     //
     private JPanel pestanaUsuarios() {
         JPanel principal = new JPanel();
-        principal.setPreferredSize(new Dimension(550, 500));
+        principal.setPreferredSize(new Dimension(650, 510));
         principal.setLayout(new BorderLayout());
         
         JPanel usuariosPanel = new JPanel();
-        usuariosPanel.setPreferredSize(new Dimension(500, 450));
+        usuariosPanel.setPreferredSize(new Dimension(600, 500));
         
         JPanel botones = new JPanel();
-        botones.setPreferredSize(new Dimension(500, 450));
-        
-        principal.add(usuariosPanel, BorderLayout.WEST);
+        JButton adduser = new JButton("Agregar Usuario");
+        JButton deluser = new JButton(" Borrar Usuario");
+        botones.add(adduser);
+        botones.add(deluser);
+        principal.add(usuariosPanel, BorderLayout.CENTER);
         principal.add(botones, BorderLayout.SOUTH);
         return principal;
     }
