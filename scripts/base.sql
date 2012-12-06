ab37b2724cbb94efe144e6e9b11aa1a3e3021391
msastre <m.sastre@ciencias.unam.mx>
2012-12-06 04:01:29 -0600

    Corrigiendo las definiciones de las tablas

--- scripts/base.sql
+++ scripts/base.sql
@@ -6,7 +6,7 @@ CREATE TABLE usuario
   ap_materno character varying(50) NOT NULL,
   email character varying(50) NOT NULL,
   fecha_nacimiento date,
-  id_password integer NOT NULL, 
+  id_password varchar(10) NOT NULL, 
   CONSTRAINT pkey_usuario PRIMARY KEY (id_usuario)
 );
 
@@ -17,9 +17,31 @@ CREATE TABLE grupo
   id_responsable integer NOT NULL,
   nombre_grupo character varying(50) NOT NULL,
   fecha_creacion date,
-  grupo_padre integer,
-  CONSTRAINT pkey_grupo PRIMARY KEY (id_grupo)
+  grupo_padre integer, 
+  CONSTRAINT pkey_grupo PRIMARY KEY (id_grupo),
+  CONSTRAINT fkey_grupo_padre FOREIGN KEY (grupo_padre)
+      REFERENCES grupo(id_grupo) MATCH FULL
+      ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE TABLE evento
+(
+  id_evento integer NOT NULL,
+  responsable integer NOT NULL,
+  fecha_inicio date,
+  fecha_final date,
+  hora_inicio time,
+  hora_final time,
+  descripcion character varying(255),
+  asistente integer UNIQUE,
+  recurso integer UNIQUE,
+  max_asistentes integer,
+  CONSTRAINT pkey_evento PRIMARY KEY (id_evento),
+  CONSTRAINT fkey_responsable FOREIGN KEY (responsable)
+      REFERENCES usuario(id_usuario) MATCH FULL
+      ON UPDATE CASCADE ON DELETE CASCADE
+);
+
 
 
 CREATE TABLE asistente
@@ -27,14 +49,14 @@ CREATE TABLE asistente
   id_usuario integer NOT NULL,
   id_evento integer NOT NULL,
   CONSTRAINT fkey_asistente FOREIGN KEY (id_usuario)
-      REFERENCES evento(id_usuario) MATCH FULL
+      REFERENCES evento(asistente) MATCH FULL
       ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 CREATE TABLE recurso
 (
-  num_inventario integer NOT NULL,
+  num_inventario integer NOT NULL UNIQUE,
   cantidad integer NOT NULL,
   CONSTRAINT fkey_recurso FOREIGN KEY (num_inventario)
       REFERENCES evento(recurso) MATCH FULL
@@ -53,23 +75,4 @@ CREATE TABLE asignacion_recursos
       REFERENCES recurso(num_inventario) MATCH FULL
       ON UPDATE CASCADE ON DELETE CASCADE
   
-);
-
-
-CREATE TABLE evento
-(
-  id_evento integer NOT NULL,
-  responsable integer NOT NULL,
-  fecha_inicio date,
-  fecha_final date,
-  hora_inicio time,
-  hora_final time,
-  descripcion character varying(255),
-  asistente integer,
-  recurso integer,
-  max_asistentes integer,
-  CONSTRAINT pkey_evento PRIMARY KEY (id_evento)
-  CONSTRAINT fkey_responsable FOREIGN KEY (responsable)
-      REFERENCES usuario(id_usuario) MATCH FULL
-      ON UPDATE CASCADE ON DELETE CASCADE
 );
